50b4622586dad03449c15d394aa8a6d7bb115e18
ayockson2500 <ayockson2500@gmail.com>
2022-04-14 13:18:58 -0700

    updat code

--- js/script.js
+++ js/script.js
@@ -4,13 +4,14 @@ const container = document.querySelector('.container');
 const myHexColors = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
 
 setInterval(myHexRandColors, 1000)
+
 function myHexRandColors() {
     const colorOneIndx = myHexColors[Math.floor(Math.random() * myHexColors.length)];
-    const colorTwoIndx = myHexColors[Math.floor(Math.random() * myHexColors.length)] ;
-    const colorThreeIndx = myHexColors[Math.floor(Math.random() * myHexColors.length)] ;
-    const colorFourIndx = myHexColors[Math.floor(Math.random() * myHexColors.length)] ;
-    const colorFiveIndx = myHexColors[Math.floor(Math.random() * myHexColors.length)] ;
-    const colorSixIndx = myHexColors[Math.floor(Math.random() * myHexColors.length)] ;
+    const colorTwoIndx = myHexColors[Math.floor(Math.random() * myHexColors.length)];
+    const colorThreeIndx = myHexColors[Math.floor(Math.random() * myHexColors.length)];
+    const colorFourIndx = myHexColors[Math.floor(Math.random() * myHexColors.length)];
+    const colorFiveIndx = myHexColors[Math.floor(Math.random() * myHexColors.length)];
+    const colorSixIndx = myHexColors[Math.floor(Math.random() * myHexColors.length)];
 
     const myColorRand = colorOneIndx + colorTwoIndx + colorThreeIndx + colorFourIndx + colorFiveIndx + colorSixIndx;
     const hexColor = "#" + myColorRand;
@@ -24,4 +25,3 @@ function myHexRandColors() {
 
 }
 myHexRandColors();
-
